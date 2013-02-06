086ea51deb92a4128a6f76c038d8f1e089d14f10
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2013-02-06 14:59:09 +0600

    Fix notifications in Android 4+ for disabled persistent icon. Close #105.

--- src/com/xabber/android/data/notification/NotificationManager.java
+++ src/com/xabber/android/data/notification/NotificationManager.java
@@ -391,7 +391,7 @@ public class NotificationManager implements OnInitializedListener,
 			chatViews.setTextViewText(R.id.text, status);
 
 			Notification notification = new Notification();
-			if (Application.SDK_INT >= 14) {
+			if (Application.SDK_INT >= 14 && SettingsManager.eventsPersistent()) {
 				// Ongoing icons are in the left side, so hide this one.
 				notification.icon = R.drawable.ic_placeholder;
 				notification.when = Long.MIN_VALUE;
@@ -429,7 +429,7 @@ public class NotificationManager implements OnInitializedListener,
 		persistentNotification.defaults = 0;
 		persistentNotification.sound = null;
 		persistentNotification.tickerText = null;
-		if (Application.SDK_INT >= 14) {
+		if (Application.SDK_INT >= 14 && SettingsManager.eventsPersistent()) {
 			// Ongoing icons are in the left side, so always use it.
 			persistentNotification.when = startTime;
 			if (messageNotifications.isEmpty()) {
