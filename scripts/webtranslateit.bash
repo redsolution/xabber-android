13e192ae5bdfba440b35fb2ecdf7023a66ec0b28
Alexander Ivanov <alexander.vl.ivanov@gmail.com>
2013-08-25 18:31:04 +0600

    Add .gitignore for webtranslateit input archive.

--- scripts/webtranslateit.bash
+++ scripts/webtranslateit.bash
@@ -1,4 +1,4 @@
-unzip -o ../../1218-xabber.zip -d ../
+unzip -o 1218-xabber.zip -d ../
 perl -p -i.bak -w -e 's/&amp;([l|g]t;)/&$1/g' $(find ../res/ -path ../res/values*.xml)
 perl -p -i.bak -w -e "s/\\\\\\\\'/\\\\'/g" $(find ../res/ -path ../res/values*.xml)
 find ../res/ -type f -name "*.bak" -exec rm -f {} \;
