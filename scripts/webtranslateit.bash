4557fd2e54dab4222559a99bffb261d1e7def446
Alexander Ivanov <alexander.vl.ivanov@gmail.com>
2013-08-25 21:54:41 +0600

    Remove unused part of webtranslateit script.

--- scripts/webtranslateit.bash
+++ scripts/webtranslateit.bash
@@ -1,5 +1,4 @@
 unzip -o ../../1218-xabber.zip -d ../
-find ../res/ -name faq.xml -delete
 perl -pi -w -e 's/&amp;([l|g]t;)/&$1/g' $(find ../res/ -path ../res/values*.xml)
 perl -pi -w -e "s/\\\\\\\\'/\\\\'/g" $(find ../res/ -path ../res/values*.xml)
 for i in `find ../res/ -type d -name value*`; do
