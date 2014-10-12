8bc71563a6b03d88b9c0089469ce29c0e6ff88d3
Alexander Ivanov <alexander.vl.ivanov@gmail.com>
2014-10-12 16:14:50 +0600

    Update translation import script.

--- scripts/webtranslateit.bash
+++ scripts/webtranslateit.bash
@@ -1,4 +1,5 @@
 unzip -o 1218-xabber.zip -d ../
 perl -p -i.bak -w -e 's/&amp;([l|g]t;)/&$1/g' $(find ../res/ -path ../res/values*.xml)
 perl -p -i.bak -w -e "s/\\\\\\\\'/\\\\'/g" $(find ../res/ -path ../res/values*.xml)
+perl -p -i.bak -w -e 's/  (<resources)/$1/g' $(find ../res/ -path ../res/values*.xml)
 find ../res/ -type f -name "*.bak" -exec rm -f {} \;
