f3eb81956dc02ab9be332dac336fae7e466c5871
Grigory Fedorov <grigory.fedorov@redsolution.ru>
2015-01-14 12:01:50 +0500

    New NotificationsSettings activity and NotificationsSettingsFragment.

---
+++ app/src/main/java/com/xabber/android/ui/preferences/NotificationsSettings.java
@@ -0,0 +1,26 @@
+package com.xabber.android.ui.preferences;
+
+
+import android.os.Bundle;
+
+import com.xabber.android.ui.helper.ManagedActivity;
+import com.xabber.androiddev.R;
+
+public class NotificationsSettings extends ManagedActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (isFinishing())
+            return;
+
+        setContentView(R.layout.activity_preferences);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.preferences_activity_container, new NotificationsSettingsFragment()).commit();
+        }
+    }
+}
