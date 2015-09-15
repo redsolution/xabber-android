17112aeca18a0ff3b143fabb40e36b0384fd2e0a
Grigory Fedorov <grigory.fedorov@redsolution.ru>
2015-09-15 12:27:04 +0500

    VCardManager: fixed vCard requests on roster load.

--- xabber/src/main/java/com/xabber/android/data/extension/vcard/VCardManager.java
+++ xabber/src/main/java/com/xabber/android/data/extension/vcard/VCardManager.java
@@ -128,7 +128,7 @@ public class VCardManager implements OnLoadListener, OnPacketListener,
         String account = accountItem.getAccount();
         if (!accountRequested.contains(account) && SettingsManager.connectionLoadVCard()) {
             String bareAddress = Jid.getBareAddress(accountItem.getRealJid());
-            if (bareAddress != null) {
+            if (bareAddress != null && !names.containsKey(bareAddress)) {
                 request(account, bareAddress);
                 accountRequested.add(account);
             }
@@ -136,7 +136,7 @@ public class VCardManager implements OnLoadListener, OnPacketListener,
 
         // Request vCards for new contacts.
         for (RosterContact contact : RosterManager.getInstance().getContacts()) {
-            if (account.equals(contact.getUser()) && !names.containsKey(contact.getUser())) {
+            if (account.equals(contact.getAccount()) && !names.containsKey(contact.getUser())) {
                 request(account, contact.getUser());
             }
         }
