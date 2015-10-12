1187dd9ee7951df9873eaeaca69097f9c4a85d7e
Grigory Fedorov <grigory.fedorov@redsolution.ru>
2015-10-12 11:37:43 +0500

    MUC users avatars requested.

--- xabber/src/main/java/com/xabber/android/data/extension/vcard/VCardManager.java
+++ xabber/src/main/java/com/xabber/android/data/extension/vcard/VCardManager.java
@@ -29,6 +29,7 @@ import com.xabber.android.data.connection.ConnectionManager;
 import com.xabber.android.data.connection.ConnectionThread;
 import com.xabber.android.data.connection.OnPacketListener;
 import com.xabber.android.data.extension.avatar.AvatarManager;
+import com.xabber.android.data.extension.muc.MUCManager;
 import com.xabber.android.data.roster.OnRosterChangedListener;
 import com.xabber.android.data.roster.OnRosterReceivedListener;
 import com.xabber.android.data.roster.PresenceManager;
@@ -266,10 +267,17 @@ public class VCardManager implements OnLoadListener, OnPacketListener,
             if (bareAddress == null) {
                 return;
             }
+
+            String addressForVcard = bareAddress;
+
+            if (MUCManager.getInstance().hasRoom(account, bareAddress)) {
+                addressForVcard = packet.getFrom();
+            }
+
             // Request vCard for new users
-            if (!names.containsKey(bareAddress)) {
+            if (!names.containsKey(addressForVcard)) {
                 if (SettingsManager.connectionLoadVCard()) {
-                    request(account, bareAddress);
+                    request(account, addressForVcard);
                 }
             }
         }
