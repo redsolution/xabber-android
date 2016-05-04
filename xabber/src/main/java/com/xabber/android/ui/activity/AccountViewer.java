aa187608356b74b4b6a8915f3f3707ddfdf4d13f
Grigory Fedorov <grigory.fedorov@redsolution.ru>
2016-05-04 16:42:59 +0500

    fix null ptr on barPainter in AccountViewer.

--- xabber/src/main/java/com/xabber/android/ui/activity/AccountViewer.java
+++ xabber/src/main/java/com/xabber/android/ui/activity/AccountViewer.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -48,6 +49,7 @@ public class AccountViewer extends ManagedActivity implements Toolbar.OnMenuItem
     private View preferencesFragmentContainer;
     private View vCardFragmentContainer;
     private Toolbar toolbar;
+    @Nullable
     private BarPainter barPainter;
     private boolean showAccountInfo;
     private TextView statusText;
@@ -293,7 +295,9 @@ public class AccountViewer extends ManagedActivity implements Toolbar.OnMenuItem
 
     @Override
     public void onColorChange(String colorName) {
-        barPainter.updateWithColorName(colorName);
-        contactTitleView.setBackgroundColor(barPainter.getAccountPainter().getAccountMainColorByColorName(colorName));
+        if (barPainter != null) {
+            barPainter.updateWithColorName(colorName);
+            contactTitleView.setBackgroundColor(barPainter.getAccountPainter().getAccountMainColorByColorName(colorName));
+        }
     }
 }
