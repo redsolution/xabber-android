48f659010f462903e4c2ac343dd3ba4b4b558060
Grigory Fedorov <grigory.fedorov@redsolution.ru>
2016-12-19 15:50:11 +0500

    change AccountJid not to create new instance every time.

--- xabber/src/main/java/com/xabber/android/data/entity/AccountJid.java
+++ xabber/src/main/java/com/xabber/android/data/entity/AccountJid.java
@@ -2,6 +2,9 @@ package com.xabber.android.data.entity;
 
 import android.support.annotation.NonNull;
 
+import com.xabber.android.data.log.LogManager;
+
+import org.jxmpp.jid.BareJid;
 import org.jxmpp.jid.DomainBareJid;
 import org.jxmpp.jid.FullJid;
 import org.jxmpp.jid.impl.JidCreate;
@@ -10,20 +13,41 @@ import org.jxmpp.jid.parts.Resourcepart;
 import org.jxmpp.stringprep.XmppStringprepException;
 
 import java.io.Serializable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class AccountJid implements Comparable<AccountJid>, Serializable {
+    private static final String LOG_TAG = AccountJid.class.getSimpleName();
+
     private final @NonNull FullJid fullJid;
 
+    private static int counter = 0;
+    private static Map<FullJid, AccountJid> instances = new ConcurrentHashMap<>();
+
+
     public static AccountJid from(Localpart localpart, DomainBareJid domainBareJid, Resourcepart resource) {
-        return new AccountJid(JidCreate.fullFrom(localpart, domainBareJid, resource));
+        return getAccountJid(JidCreate.fullFrom(localpart, domainBareJid, resource));
     }
 
     public static AccountJid from(@NonNull String string) throws XmppStringprepException {
-        return new AccountJid(JidCreate.fullFrom(string));
+        return getAccountJid(JidCreate.fullFrom(string));
     }
 
     private AccountJid(@NonNull FullJid fullJid) {
         this.fullJid = fullJid;
+        counter++;
+    }
+
+    private static AccountJid getAccountJid(@NonNull FullJid fullJid) {
+        AccountJid accountJid = instances.get(fullJid);
+        if (accountJid != null) {
+            return accountJid;
+        } else {
+            AccountJid newAccountJid = new AccountJid(fullJid);
+            instances.put(fullJid, newAccountJid);
+            LogManager.i(LOG_TAG, "AccountJid created " + counter + " / " + instances.size() + " " + fullJid);
+            return newAccountJid;
+        }
     }
 
     public @NonNull FullJid getFullJid() {
