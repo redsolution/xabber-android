dd1febfdb58b315fe433585a4f638a16ea7c2a68
Grigory Fedorov <grigory.fedorov@redsolution.ru>
2017-01-10 12:33:38 +0500

    remove catch of NPE and IllegalArgumentException - Smack updated.

--- xabber/src/main/java/com/xabber/android/data/connection/ConnectionThread.java
+++ xabber/src/main/java/com/xabber/android/data/connection/ConnectionThread.java
@@ -88,9 +88,6 @@ class ConnectionThread {
             });
         } catch (XMPPException | SmackException | IOException | InterruptedException e) {
             LogManager.exception(this, e);
-        } catch (NullPointerException | IllegalArgumentException e) {
-            // TODO - should be fixed either in Smack or Xabber
-            LogManager.exception(this, e);
         }
     }
 
