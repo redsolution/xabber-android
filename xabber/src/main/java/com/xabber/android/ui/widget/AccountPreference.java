4d9ef0cdbff13277c58d9e90b0fcf0981864a966
Valery Miller <valera_miller94@mail.ru>
2017-07-27 16:53:50 +0500

    Added custom Account Preference Show account status in preferences

---
+++ xabber/src/main/java/com/xabber/android/ui/widget/AccountPreference.java
@@ -0,0 +1,63 @@
+package com.xabber.android.ui.widget;
+
+import android.content.Context;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.xabber.android.R;
+import com.xabber.android.data.xaccount.XabberAccount;
+import com.xabber.android.data.xaccount.XabberAccountManager;
+
+/**
+ * Created by valery.miller on 27.07.17.
+ */
+
+public class AccountPreference extends Preference {
+
+    public AccountPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+        super.onCreateView(parent);
+        LayoutInflater li = (LayoutInflater)getContext().getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+        return li.inflate( R.layout.preference_account, parent, false);
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+        LinearLayout llAccountInfo = (LinearLayout) view.findViewById(R.id.accountInfo);
+        LinearLayout llNoAccount = (LinearLayout) view.findViewById(R.id.noAccount);
+        TextView tvAccountName = (TextView) view.findViewById(R.id.tvAccountName);
+        TextView tvAccountEmail = (TextView) view.findViewById(R.id.tvAccountEmail);
+        XabberAccount account = XabberAccountManager.getInstance().getAccount();
+
+        if (account != null) {
+            llAccountInfo.setVisibility(View.VISIBLE);
+            llNoAccount.setVisibility(View.GONE);
+            if (XabberAccount.STATUS_NOT_CONFIRMED.equals(account.getAccountStatus())) {
+                tvAccountName.setText(R.string.account_not_confirmed);
+                tvAccountEmail.setText(account.getEmails().get(0).getEmail());
+            }
+            if (XabberAccount.STATUS_CONFIRMED.equals(account.getAccountStatus())) {
+                tvAccountName.setText(R.string.register_not_completed);
+                tvAccountEmail.setText(account.getEmails().get(0).getEmail());
+            }
+            if (XabberAccount.STATUS_REGISTERED.equals(account.getAccountStatus())) {
+                String accountName = account.getFirstName() + " " + account.getLastName();
+                tvAccountName.setText(accountName);
+                tvAccountEmail.setText(account.getEmails().get(0).getEmail());
+            }
+        } else {
+            llAccountInfo.setVisibility(View.GONE);
+            llNoAccount.setVisibility(View.VISIBLE);
+        }
+    }
+}
