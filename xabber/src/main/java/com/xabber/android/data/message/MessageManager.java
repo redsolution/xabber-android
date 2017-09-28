92fe38eb5e9b81801219ee9fed56627ea4101ef1
Valery Miller <valera_miller94@mail.ru>
2017-09-28 14:16:29 +0500

    Temporary disabled notification about status changed

--- xabber/src/main/java/com/xabber/android/data/message/MessageManager.java
+++ xabber/src/main/java/com/xabber/android/data/message/MessageManager.java
@@ -771,47 +771,49 @@ public class MessageManager implements OnLoadListener, OnPacketListener, OnDisco
 
     @Override
     public void onStatusChanged(AccountJid account, final UserJid user, final String statusText) {
-        if (isStatusTrackingEnabled(account, user)) {
-            final AbstractChat chat = getChat(account, user);
-            if (chat != null) {
-                Application.getInstance().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // fix for saving to realm
-                        String text;
-                        if (statusText != null) {
-                            if (!statusText.isEmpty() && statusText.length() > 0) text = statusText;
-                            else text = " ";
-                        } else text = " ";
-                        // create new action
-                        chat.newAction(user.getJid().getResourceOrNull(), text, ChatAction.status);
-                    }
-                });
-            }
-        }
+        // temporary disabled
+//        if (isStatusTrackingEnabled(account, user)) {
+//            final AbstractChat chat = getChat(account, user);
+//            if (chat != null) {
+//                Application.getInstance().runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        // fix for saving to realm
+//                        String text;
+//                        if (statusText != null) {
+//                            if (!statusText.isEmpty() && statusText.length() > 0) text = statusText;
+//                            else text = " ";
+//                        } else text = " ";
+//                        // create new action
+//                        chat.newAction(user.getJid().getResourceOrNull(), text, ChatAction.status);
+//                    }
+//                });
+//            }
+//        }
     }
 
     @Override
     public void onStatusChanged(AccountJid account, final UserJid user, final StatusMode statusMode, final String statusText) {
-        if (isStatusTrackingEnabled(account, user)) {
-            final AbstractChat chat = getChat(account, user);
-            if (chat != null) {
-                Application.getInstance().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // fix for saving to realm
-                        String text;
-                        if (statusText != null) {
-                            if (!statusText.isEmpty() && statusText.length() > 0) text = statusText;
-                            else text = " ";
-                        } else text = " ";
-                        // create new action
-                        chat.newAction(user.getJid().getResourceOrNull(),
-                                text, ChatAction.getChatAction(statusMode));
-                    }
-                });
-            }
-        }
+        // temporary disabled
+//        if (isStatusTrackingEnabled(account, user)) {
+//            final AbstractChat chat = getChat(account, user);
+//            if (chat != null) {
+//                Application.getInstance().runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        // fix for saving to realm
+//                        String text;
+//                        if (statusText != null) {
+//                            if (!statusText.isEmpty() && statusText.length() > 0) text = statusText;
+//                            else text = " ";
+//                        } else text = " ";
+//                        // create new action
+//                        chat.newAction(user.getJid().getResourceOrNull(),
+//                                text, ChatAction.getChatAction(statusMode));
+//                    }
+//                });
+//            }
+//        }
     }
 
     public void acceptMucPrivateChat(AccountJid account, UserJid user) throws UserJid.UserJidCreateException {
