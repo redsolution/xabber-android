d69f3b94054680f23fbc0bc308f678f5a5eb692a
Valery Miller <valera_miller94@mail.ru>
2017-09-29 11:31:32 +0500

    FIxed last register screen

--- xabber/src/main/java/com/xabber/android/ui/fragment/XabberAccountLastFragment.java
+++ xabber/src/main/java/com/xabber/android/ui/fragment/XabberAccountLastFragment.java
@@ -2,6 +2,7 @@ package com.xabber.android.ui.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,6 +26,9 @@ public class XabberAccountLastFragment extends Fragment {
 
     private static final String LOG_TAG = XabberAccountLastFragment.class.getSimpleName();
 
+    private TextView tvAccountName;
+    private TextView tvSignType;
+
     private Button btnYes;
     private Button btnNo;
     private TextView tvDescription;
@@ -42,6 +46,9 @@ public class XabberAccountLastFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        tvAccountName = (TextView) view.findViewById(R.id.tvAccountName);
+        tvSignType = (TextView) view.findViewById(R.id.tvSignType);
 
         tvDescription = (TextView) view.findViewById(R.id.tvDescription);
         btnYes = (Button) view.findViewById(R.id.btnYes);
@@ -71,6 +78,7 @@ public class XabberAccountLastFragment extends Fragment {
             XMPPUser user = account.getXmppUsers().get(0);
             if (user != null) jid = user.getUsername() + "@" + user.getHost();
             tvDescription.setText(getString(R.string.complete_register_summary, jid));
+            updateData(account);
         }
     }
 
@@ -78,5 +86,43 @@ public class XabberAccountLastFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         compositeSubscription.clear();
+    }
+
+    public void updateData(@NonNull XabberAccount account) {
+        if (account.getEmails().size() > 0) {
+            // if registered via email
+            tvAccountName.setText(account.getEmails().get(0).getEmail());
+            tvSignType.setText(R.string.signed_up_email);
+        } else {
+            // if registered via social binding
+            String firstName = account.getFirstName();
+            String lastName = account.getLastName();
+            String name = "";
+            if (firstName != null) {
+                name = name + firstName;
+            }
+            if (lastName != null) {
+                name = name + " " + lastName;
+            }
+            if (!name.isEmpty()) tvAccountName.setText(name);
+
+            if (account.getSocialBindings().size() > 0) {
+                switch (account.getSocialBindings().get(0).getProvider()) {
+                    case "google":
+                        tvSignType.setText(R.string.signed_up_google);
+                        break;
+                    case "facebook":
+                        tvSignType.setText(R.string.signed_up_facebook);
+                        break;
+                    case "twitter":
+                        tvSignType.setText(R.string.signed_up_twitter);
+                        break;
+                    case "github":
+                        tvSignType.setText(R.string.signed_up_github);
+                        break;
+                }
+            }
+        }
+
     }
 }
