1bf474639a21d2328fce844d95cb776a1cd2264f
Valery Miller <valera_miller94@mail.ru>
2017-12-27 14:39:35 +0500

    Visual changes in add conference screen

--- xabber/src/main/java/com/xabber/android/ui/fragment/ConferenceAddFragment.java
+++ xabber/src/main/java/com/xabber/android/ui/fragment/ConferenceAddFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -67,7 +68,7 @@ public class ConferenceAddFragment extends Fragment {
         int h = accountAvatar.getIntrinsicHeight();
         int w = accountAvatar.getIntrinsicWidth();
         accountAvatar.setBounds( 0, 0, w, h );
-        ((TextView) view.findViewById(R.id.muc_account_jid)).setCompoundDrawables(accountAvatar, null, null, null);
+        ((ImageView) view.findViewById(R.id.ivAvatar)).setImageDrawable(accountAvatar);
 
 
         nickView = (EditText) view.findViewById(R.id.muc_nick);
