7f1e46571e24ddeac344cc7bbe9a2a145c4c50f4
Valery Miller <valera_miller94@mail.ru>
2018-02-07 11:17:09 +0500

    Contact list refactoring

--- xabber/src/main/java/com/xabber/android/presentation/ui/contactlist/ContactListFragment.java
+++ xabber/src/main/java/com/xabber/android/presentation/ui/contactlist/ContactListFragment.java
@@ -13,6 +13,8 @@ import com.xabber.android.R;
 import com.xabber.android.data.entity.AccountJid;
 import com.xabber.android.presentation.mvp.contactlist.ContactListPresenter;
 import com.xabber.android.presentation.mvp.contactlist.ContactListView;
+import com.xabber.android.presentation.ui.contactlist.viewobjects.AccountVO;
+import com.xabber.android.ui.activity.ContactListActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +26,8 @@ import eu.davidea.flexibleadapter.items.IFlexible;
  * Created by valery.miller on 02.02.18.
  */
 
-public class ContactListFragment extends Fragment implements ContactListView {
+public class ContactListFragment extends Fragment implements ContactListView,
+        FlexibleAdapter.OnStickyHeaderChangeListener {
 
     public static final String ACCOUNT_JID = "account_jid";
 
@@ -59,6 +62,7 @@ public class ContactListFragment extends Fragment implements ContactListView {
 
         adapter.setSwipeEnabled(true);
         adapter.expandItemsAtStartUp();
+        adapter.addListener(this);
 
         return view;
     }
@@ -67,6 +71,7 @@ public class ContactListFragment extends Fragment implements ContactListView {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         presenter = ContactListPresenter.getInstance(getActivity());
+        ((ContactListActivity)getActivity()).setStatusBarColor();
     }
 
     @Override
@@ -80,5 +85,18 @@ public class ContactListFragment extends Fragment implements ContactListView {
         this.items.clear();
         this.items.addAll(items);
         adapter.updateDataSet(items);
+    }
+
+    /**
+     * Callback. When new header becomes sticky - change color of status bar.
+     */
+    @Override
+    public void onStickyHeaderChange(int newPosition, int oldPosition) {
+        if (newPosition > -1 && adapter.getItemCount() > newPosition) {
+            IFlexible item = adapter.getItem(newPosition);
+            if (item instanceof AccountVO)
+                ((ContactListActivity)getActivity()).setStatusBarColor(((AccountVO)item).getAccountJid()); // account color
+            else ((ContactListActivity)getActivity()).setStatusBarColor(); // main color
+        }
     }
 }
