4cafed8e7ed731b96933d8afc43e52e5f1a39048
Valery Miller <valera_miller94@mail.ru>
2018-03-23 18:30:23 +0500

    Fixed Realm migration

--- xabber/src/main/java/com/xabber/android/data/database/RealmManager.java
+++ xabber/src/main/java/com/xabber/android/data/database/RealmManager.java
@@ -23,12 +23,13 @@ import io.realm.FieldAttribute;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import io.realm.RealmMigration;
+import io.realm.RealmObjectSchema;
 import io.realm.RealmSchema;
 import io.realm.annotations.RealmModule;
 
 public class RealmManager {
     private static final String REALM_DATABASE_NAME = "realm_database.realm";
-    private static final int REALM_DATABASE_VERSION = 11;
+    private static final int REALM_DATABASE_VERSION = 12;
     private static final String LOG_TAG = RealmManager.class.getSimpleName();
     private final RealmConfiguration realmConfiguration;
 
@@ -187,12 +188,25 @@ public class RealmManager {
                         if (oldVersion == 9) {
                             schema.get(XabberAccountRealm.class.getSimpleName())
                                     .addField("language", String.class);
+
+                            // do not delete
+                            oldVersion++;
+                            oldVersion++;
                         }
 
-                        if (oldVersion == 10) {
-                            schema.get(XabberAccountRealm.class.getSimpleName())
-                                    .addField("phone", String.class)
-                                    .addField("needToVerifyPhone", boolean.class);
+                        if (oldVersion == 11) {
+                            RealmObjectSchema accountRealmSchema =
+                                    schema.get(XabberAccountRealm.class.getSimpleName());
+
+                            if (!accountRealmSchema.hasField("phone")) {
+                                accountRealmSchema.addField("phone", String.class);
+                            }
+
+                            if (!accountRealmSchema.hasField("needToVerifyPhone")) {
+                                accountRealmSchema.addField("needToVerifyPhone", boolean.class);
+                            }
+
+                            oldVersion++;
                         }
                     }
                 })
