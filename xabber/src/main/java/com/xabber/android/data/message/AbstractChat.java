83ea3d78d874104a0bb9cc8c1277be9ccf59231f
Valery Miller <valera_miller94@mail.ru>
2018-05-04 18:11:25 +0500

    Setup file size on create FileMessage

--- xabber/src/main/java/com/xabber/android/data/message/AbstractChat.java
+++ xabber/src/main/java/com/xabber/android/data/message/AbstractChat.java
@@ -374,6 +374,7 @@ public abstract class AbstractChat extends BaseEntity implements RealmChangeList
                 messageItem.setUser(user);
                 messageItem.setText(file.getName());
                 messageItem.setFilePath(file.getPath());
+                messageItem.setFileSize(file.length());
                 messageItem.setIsImage(FileManager.fileIsImage(file));
                 messageItem.setTimestamp(System.currentTimeMillis());
                 messageItem.setRead(true);
