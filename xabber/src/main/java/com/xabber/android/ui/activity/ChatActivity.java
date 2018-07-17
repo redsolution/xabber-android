9b44f49839d4d31b0446f4dbe52ef628f29b797d
Valery Miller <valera_miller94@mail.ru>
2018-07-17 17:37:52 +0500

    Fixed blockable method in ChatActivity

--- xabber/src/main/java/com/xabber/android/ui/activity/ChatActivity.java
+++ xabber/src/main/java/com/xabber/android/ui/activity/ChatActivity.java
@@ -702,7 +702,7 @@ public class ChatActivity extends ManagedActivity implements OnContactChangedLis
     @Override
     public void onBlockedListChanged(AccountJid account) {
         // if chat of blocked contact is currently opened, it should be closed
-        final Collection<UserJid> blockedContacts = BlockingManager.getInstance().getBlockedContacts(account);
+        final Collection<UserJid> blockedContacts = BlockingManager.getInstance().getCachedBlockedContacts(account);
         if (blockedContacts.contains(user)) {
             close();
         }
