36952bae9f508ccc097ee71a1c3c08c2b3bc2582
Valery Miller <valera_miller94@mail.ru>
2018-09-24 15:23:38 +0500

    Changes in tutorial screen - added new slides

--- xabber/src/main/java/com/xabber/android/ui/adapter/TutorialAdapter.java
+++ xabber/src/main/java/com/xabber/android/ui/adapter/TutorialAdapter.java
@@ -14,7 +14,7 @@ import com.xabber.android.ui.fragment.TutorialFragment;
  */
 
 public class TutorialAdapter extends FragmentPagerAdapter {
-    private static int PAGE_NUMBERS = 4;
+    private static int PAGE_NUMBERS = 9;
 
     public TutorialAdapter(FragmentManager fm) {
         super(fm);
@@ -31,6 +31,16 @@ public class TutorialAdapter extends FragmentPagerAdapter {
                 return TutorialFragment.newInstance(R.string.tutorial_title_3, R.string.tutorial_description_3, R.drawable.art_tutorial_3);
             case 3:
                 return TutorialFragment.newInstance(R.string.tutorial_title_4, R.string.tutorial_description_4, R.drawable.art_tutorial_4);
+            case 4:
+                return TutorialFragment.newInstance(R.string.tutorial_title_5, R.string.tutorial_description_5, R.drawable.art_tutorial_1);
+            case 5:
+                return TutorialFragment.newInstance(R.string.tutorial_title_6, R.string.tutorial_description_6, R.drawable.art_tutorial_2);
+            case 6:
+                return TutorialFragment.newInstance(R.string.tutorial_title_7, R.string.tutorial_description_7, R.drawable.art_tutorial_3);
+            case 7:
+                return TutorialFragment.newInstance(R.string.tutorial_title_8, R.string.tutorial_description_8, R.drawable.art_tutorial_4);
+            case 8:
+                return TutorialFragment.newInstance(R.string.tutorial_title_9, R.string.tutorial_description_9, R.drawable.art_tutorial_1);
             default:
                 return null;
         }
