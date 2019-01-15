dc35322c3b14d32426d5de80868f0eaa009a52e7
Valery Miller <valera_miller94@mail.ru>
2019-01-15 16:09:22 +0500

    a href links now clickable in crowdfunding chat

--- xabber/src/main/java/com/xabber/android/ui/adapter/chat/CrowdfundingChatAdapter.java
+++ xabber/src/main/java/com/xabber/android/ui/adapter/chat/CrowdfundingChatAdapter.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -91,6 +92,9 @@ public class CrowdfundingChatAdapter extends RealmRecyclerViewAdapter<Crowdfundi
         String text = message.getMessageForCurrentLocale();
         if (text == null) text = "";
         holder.messageText.setText(Html.fromHtml(text));
+        // to avoid bug - https://issuetracker.google.com/issues/36907309
+        holder.messageText.setAutoLinkMask(0);
+        holder.messageText.setMovementMethod(LinkMovementMethod.getInstance());
 
         // text or image
         if (FileManager.isImageUrl(text)) {
