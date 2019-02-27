51ba9d02519005b6f4e0b2313dbec28d3c21213c
Valery Miller <valera_miller94@mail.ru>
2019-02-27 18:18:23 +0500

    Fixed forwarding messages. The issue was a async saving to db.

--- xabber/src/main/java/com/xabber/android/data/message/ForwardManager.java
+++ xabber/src/main/java/com/xabber/android/data/message/ForwardManager.java
@@ -1,5 +1,6 @@
 package com.xabber.android.data.message;
 
+import com.xabber.android.data.database.MessageDatabaseManager;
 import com.xabber.android.data.database.messagerealm.ForwardId;
 import com.xabber.android.data.database.messagerealm.MessageItem;
 import com.xabber.android.data.entity.AccountJid;
@@ -12,13 +13,14 @@ import org.jivesoftware.smack.packet.Stanza;
 
 import java.util.List;
 
+import io.realm.Realm;
 import io.realm.RealmList;
 
 public class ForwardManager {
 
     public static void forwardMessage(List<String> messages, AccountJid account, UserJid user, String text) {
-        AbstractChat chat = MessageManager.getInstance().getOrCreateChat(account, user);
-        MessageItem messageItem = chat.createNewMessageItem(text);
+        final AbstractChat chat = MessageManager.getInstance().getOrCreateChat(account, user);
+        final MessageItem messageItem = chat.createNewMessageItem(text);
 
         RealmList<ForwardId> ids = new RealmList<>();
 
@@ -27,8 +29,15 @@ public class ForwardManager {
         }
 
         messageItem.setForwardedIds(ids);
-        chat.saveMessageItem(true, messageItem);
-        chat.sendMessages();
+
+        MessageDatabaseManager.getInstance().getRealmUiThread()
+                .executeTransactionAsync(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        realm.copyToRealm(messageItem);
+                        chat.sendMessages();
+                    }
+                });
         EventBus.getDefault().post(new NewMessageEvent());
     }
 
