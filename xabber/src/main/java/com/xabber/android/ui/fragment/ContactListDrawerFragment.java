6486c38ac524ae717a8b4cbf78e30855d23a2d42
Valery Miller <valera_miller94@mail.ru>
2019-07-12 16:21:04 +0500

    Migration to androidx

--- xabber/src/main/java/com/xabber/android/ui/fragment/ContactListDrawerFragment.java
+++ xabber/src/main/java/com/xabber/android/ui/fragment/ContactListDrawerFragment.java
@@ -2,15 +2,14 @@ package com.xabber.android.ui.fragment;
 
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
