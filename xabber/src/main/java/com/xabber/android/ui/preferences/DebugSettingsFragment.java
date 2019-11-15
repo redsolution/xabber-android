35a1fc532e03ba354c09a5a0acc1b9d7597813cb
Vitaly Popov <perdovorotov@gmail.com>
2019-11-15 17:14:23 +0500

    Implemented writing messages to log

--- xabber/src/main/java/com/xabber/android/ui/preferences/DebugSettingsFragment.java
+++ xabber/src/main/java/com/xabber/android/ui/preferences/DebugSettingsFragment.java
@@ -30,7 +30,7 @@ public class DebugSettingsFragment extends android.preference.PreferenceFragment
 
         PreferenceScreen preferenceScreen = getPreferenceScreen();
 
-        preferenceScreen.removePreference(preferenceScreen.findPreference(getString(R.string.debug_log_key)));
+        //preferenceScreen.removePreference(preferenceScreen.findPreference(getString(R.string.debug_log_key)));
         preferenceScreen.removePreference(preferenceScreen.findPreference(getString(R.string.cache_clear_key)));
         preferenceScreen.removePreference(preferenceScreen.findPreference(getString(R.string.debug_connection_errors_key)));
 
