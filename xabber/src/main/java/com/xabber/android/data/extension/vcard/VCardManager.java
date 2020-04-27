59739df0f96fbf0406583e26e3e7653d8eb3ba1b
Vitaly Popov <perdovorotov@gmail.com>
2020-04-27 17:31:55 +0500

    Deleted CustomVcardManager

--- xabber/src/main/java/com/xabber/android/data/extension/vcard/VCardManager.java
+++ xabber/src/main/java/com/xabber/android/data/extension/vcard/VCardManager.java
@@ -42,10 +42,13 @@ import com.xabber.xmpp.vcard.VCardProperty;
 
 import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.IQ;
 import org.jivesoftware.smack.packet.IQ.Type;
 import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.packet.Stanza;
+import org.jivesoftware.smack.packet.id.StanzaIdUtil;
 import org.jxmpp.jid.BareJid;
 import org.jxmpp.jid.Jid;
 
@@ -341,9 +344,6 @@ public class VCardManager implements OnLoadListener, OnPacketListener,
             return;
         }
 
-        final CustomVCardManager vCardManager
-                = CustomVCardManager.getInstanceFor(accountItem.getConnection());
-
         if (!accountItem.getConnection().isAuthenticated()) {
             onVCardFailed(account, srcUser);
             return;
@@ -363,7 +363,7 @@ public class VCardManager implements OnLoadListener, OnPacketListener,
         if (bareJid != null) {
             vCardRequests.add(srcUser);
             try {
-                vCardManager.sendVCardRequest(bareJid);
+                sendVCardRequest(accountItem.getConnection(), bareJid);
             } catch (SmackException.NotConnectedException e) {
                 LogManager.exception(this, e);
                 LogManager.w(this, "Error getting vCard: " + e.getMessage());
@@ -382,6 +382,12 @@ public class VCardManager implements OnLoadListener, OnPacketListener,
         }
     }
 
+    private void sendVCardRequest(XMPPConnection connection, Jid jid) throws SmackException.NotConnectedException, InterruptedException {
+        VCard vcardRequest = new VCard();
+        vcardRequest.setTo(jid);
+        connection.sendStanza(vcardRequest);
+    }
+
     public void saveVCard(final AccountJid account, final VCard vCard) {
         AccountItem accountItem = AccountManager.getInstance().getAccount(account);
         if (accountItem == null) {
@@ -390,7 +396,6 @@ public class VCardManager implements OnLoadListener, OnPacketListener,
         }
 
         final AbstractXMPPConnection xmppConnection = accountItem.getConnection();
-        final CustomVCardManager vCardManager = CustomVCardManager.getInstanceFor(xmppConnection);
 
         Application.getInstance().runInBackgroundNetworkUserRequest(new Runnable() {
             @Override
@@ -402,7 +407,7 @@ public class VCardManager implements OnLoadListener, OnPacketListener,
 
                 vCardSaveRequests.add(account);
                 try {
-                    vCardManager.saveVCard(vCard);
+                    sendUpdateAccountVcardStanza(xmppConnection, vCard);
                     String avatarHash = null;
 
                     try {
@@ -438,6 +443,26 @@ public class VCardManager implements OnLoadListener, OnPacketListener,
             }
         });
     }
+
+    /**
+     * Save this vCard for the user connected by 'connection'. XMPPConnection should be authenticated
+     * and not anonymous.
+     *
+     * @throws XMPPException.XMPPErrorException thrown if there was an issue setting the VCard in the server.
+     * @throws SmackException.NoResponseException if there was no response from the server.
+     * @throws SmackException.NotConnectedException
+     * @throws InterruptedException
+     */
+    private void sendUpdateAccountVcardStanza(XMPPConnection connection, VCard vcard) throws SmackException.NoResponseException, XMPPException.XMPPErrorException, SmackException.NotConnectedException, InterruptedException {
+        // XEP-54 § 3.2 "A user may publish or update his or her vCard by sending an IQ of type "set" with no 'to' address…"
+        vcard.setTo((Jid) null);
+        vcard.setType(IQ.Type.set);
+        // Also make sure to generate a new stanza id (the given vcard could be a vcard result), in which case we don't
+        // want to use the same stanza id again (although it wouldn't break if we did)
+        vcard.setStanzaId(StanzaIdUtil.newStanzaId());
+        connection.createStanzaCollectorAndSend(vcard).nextResultOrThrow();
+    }
+
 
     public boolean isVCardRequested(Jid user) {
         return vCardRequests.contains(user.asBareJid());
