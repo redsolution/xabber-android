9722c7d96ee57677e7d78a762785bbccabf20911
Vitaly Popov <perdovorotov@gmail.com>
2020-09-02 16:59:47 +0500

    Fixed non null calls at chat item views

--- xabber/src/main/java/com/xabber/android/ui/fragment/chatListFragment/ChatViewHolder.kt
+++ xabber/src/main/java/com/xabber/android/ui/fragment/chatListFragment/ChatViewHolder.kt
@@ -14,14 +14,14 @@ class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
     var rosterStatus: Int? = null
     var accountColorIndicator: Int? = null
 
-    val avatarIV = itemView.findViewById<ImageView>(R.id.ivAvatar)
-    val statusIV = itemView.findViewById<ImageView>(R.id.ivStatus)
-    val contactNameTV = itemView.findViewById<TextView>(R.id.tvContactName)
-    val messageTextTV = itemView.findViewById<EmojiconTextView>(R.id.tvMessageText)
-    val timeTV = itemView.findViewById<TextView>(R.id.tvTime)
-    val messageStatusTV = itemView.findViewById<ImageView>(R.id.ivMessageStatus)
-    val unreadCountTV = itemView.findViewById<TextView>(R.id.tvUnreadCount)
-    val accountColorIndicatorBackView = itemView.findViewById<View>(R.id.accountColorIndicatorBack)
-    val accountColorIndicatorView = itemView.findViewById<View>(R.id.accountColorIndicator)
+    val avatarIV: ImageView = itemView.findViewById(R.id.ivAvatar)
+    val statusIV: ImageView = itemView.findViewById(R.id.ivStatus)
+    val contactNameTV: TextView = itemView.findViewById(R.id.tvContactName)
+    val messageTextTV: EmojiconTextView = itemView.findViewById(R.id.tvMessageText)
+    val timeTV: TextView = itemView.findViewById(R.id.tvTime)
+    val messageStatusTV: ImageView = itemView.findViewById(R.id.ivMessageStatus)
+    val unreadCountTV: TextView = itemView.findViewById(R.id.tvUnreadCount)
+    val accountColorIndicatorBackView: View = itemView.findViewById<View>(R.id.accountColorIndicatorBack)
+    val accountColorIndicatorView: View = itemView.findViewById<View>(R.id.accountColorIndicator)
 
 }
