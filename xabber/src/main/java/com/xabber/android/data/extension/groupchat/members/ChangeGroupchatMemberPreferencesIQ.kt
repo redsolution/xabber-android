68002b1233ad700e5266118c1c66e71ad0a18652
Vitaly Popov <perdovorotov@gmail.com>
2020-09-29 17:59:18 +0500

    Implemented IQ for changing groupchat member preferences

---
+++ xabber/src/main/java/com/xabber/android/data/extension/groupchat/members/ChangeGroupchatMemberPreferencesIQ.kt
@@ -0,0 +1,60 @@
+package com.xabber.android.data.extension.groupchat.members
+
+import com.xabber.android.data.entity.ContactJid
+import com.xabber.android.data.message.chat.groupchat.GroupchatManager
+import com.xabber.xmpp.SimpleNamedElement
+import org.jivesoftware.smack.packet.ExtensionElement
+import org.jivesoftware.smack.packet.NamedElement
+import org.jivesoftware.smack.util.XmlStringBuilder
+
+class ChangeGroupchatMemberPreferencesIQ(val groupchatJid: ContactJid, val memberId: String,
+                                         val badge: String? = null, val nickname: String? = null)
+    : GroupchatAbstractMembersIQ() {
+
+    init {
+        this.type = Type.set
+        this.to = groupchatJid.bareJid
+    }
+
+    companion object {
+        const val BADGE_ELEMENT = "badge"
+        const val NICKNAME_ELEMENT = "nickname"
+    }
+
+    override fun getIQChildElementBuilder(xml: IQChildElementXmlStringBuilder) = xml.apply {
+        rightAngleBracket()
+        val elementsList = mutableListOf<NamedElement>()
+
+        if (badge == null && nickname.isNullOrBlank())
+            throw IllegalStateException("There are should be at least one argument")
+
+        if (badge != null) elementsList.add(SimpleNamedElement(BADGE_ELEMENT, badge))
+        if (!nickname.isNullOrBlank()) elementsList.add(SimpleNamedElement(NICKNAME_ELEMENT, nickname))
+
+        append(UserExtensionElement(memberId, elementsList).toXML())
+    }
+
+    private class UserExtensionElement(val memberId: String, val elements: List<NamedElement>) : ExtensionElement {
+
+        companion object {
+            const val USER_ELEMENT = "user"
+            const val ID_ATTRIBUTE = "id"
+        }
+
+        override fun getNamespace() = GroupchatManager.NAMESPACE
+
+        override fun getElementName() = USER_ELEMENT
+
+        override fun toXML() = XmlStringBuilder().apply {
+            halfOpenElement(elementName)
+            attribute(ID_ATTRIBUTE, memberId)
+            xmlnsAttribute(namespace)
+            rightAngleBracket()
+            for (element in elements)
+                append(element.toXML())
+            closeElement(USER_ELEMENT)
+        }
+
+    }
+
+}
---
+++ xabber/src/test/java/com/xabber/android/data/extension/groupchat/ChangeGroupchatMemberPreferencesIqTest.kt
@@ -0,0 +1,43 @@
+package com.xabber.android.data.extension.groupchat
+
+import com.xabber.android.data.TestApplication
+import com.xabber.android.data.entity.ContactJid
+import com.xabber.android.data.extension.groupchat.members.ChangeGroupchatMemberPreferencesIQ
+import org.junit.Assert
+import org.junit.Test
+import org.robolectric.annotation.Config
+
+@Config(sdk = [28], application = TestApplication::class)
+class ChangeGroupchatMemberPreferencesIqTest {
+
+    @Test
+    fun testIqCreatingSetBadge() {
+        val reference = "<iq to='groupchat@otherdomain.com' id='iqId' type='set'><query xmlns='http://xabber.com/protocol/groupchat#members'><user id='memberId' xmlns='http://xabber.com/protocol/groupchat'><badge>badge!</badge></user></query></iq>"
+        val groupchatJid = ContactJid.from("groupchat@otherdomain.com")
+
+        val memberId = "memberId"
+        val badge = "badge!"
+        val iqId = "iqId"
+
+        val iq = ChangeGroupchatMemberPreferencesIQ(groupchatJid,
+                memberId, badge, null).apply { stanzaId = iqId }
+
+        Assert.assertEquals(reference, iq.toXML().toString())
+    }
+
+    @Test
+    fun testIqCreatingSetNickname() {
+        val reference = "<iq to='groupchat@otherdomain.com' id='iqId' type='set'><query xmlns='http://xabber.com/protocol/groupchat#members'><user id='memberId' xmlns='http://xabber.com/protocol/groupchat'><nickname>nickname!</nickname></user></query></iq>"
+        val groupchatJid = ContactJid.from("groupchat@otherdomain.com")
+
+        val memberId = "memberId"
+        val nickname = "nickname!"
+        val iqId = "iqId"
+
+        val iq = ChangeGroupchatMemberPreferencesIQ(groupchatJid,
+                memberId, null, nickname).apply { stanzaId = iqId }
+
+        Assert.assertEquals(reference, iq.toXML().toString())
+    }
+
+}
