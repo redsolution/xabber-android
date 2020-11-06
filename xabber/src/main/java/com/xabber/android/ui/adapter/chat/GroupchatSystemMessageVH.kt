7ad46ac586d76e09e28f7b6298a58f9f8275a39e
Vitaly Popov <perdovorotov@gmail.com>
2020-11-06 16:42:22 +0500

    Center text at group system messages

--- xabber/src/main/java/com/xabber/android/ui/adapter/chat/GroupchatSystemMessageVH.kt
+++ xabber/src/main/java/com/xabber/android/ui/adapter/chat/GroupchatSystemMessageVH.kt
@@ -1,5 +1,6 @@
 package com.xabber.android.ui.adapter.chat
 
+import android.view.Gravity
 import android.view.View
 import com.xabber.android.data.database.realmobjects.MessageRealmObject
 import com.xabber.android.utils.StringUtils
@@ -8,6 +9,7 @@ class GroupchatSystemMessageVH(val itemView: View): BasicMessageVH(itemView) {
 
     fun bind(messageRealmObject: MessageRealmObject) {
         messageText.text = messageRealmObject.text
+        messageText.gravity = Gravity.CENTER_HORIZONTAL
         date = StringUtils.getDateStringForMessage(messageRealmObject.timestamp)
     }
 }
