07340a782c636895ce9bebb138d37f6652e3e36c
Vitaly Popov <perdovorotov@gmail.com>
2020-11-09 19:35:37 +0500

    Group statuses fixes

--- xabber/src/main/java/com/xabber/android/ui/adapter/groups/GroupStatusAdapter.kt
+++ xabber/src/main/java/com/xabber/android/ui/adapter/groups/GroupStatusAdapter.kt
@@ -3,15 +3,17 @@ package com.xabber.android.ui.adapter.groups
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.xabber.android.R
 import com.xabber.android.data.account.StatusMode
+import com.xabber.android.data.message.chat.groupchat.GroupChat
+import com.xabber.android.data.message.chat.groupchat.GroupchatPrivacyType
+import com.xabber.android.data.roster.StatusBadgeSetupHelper
 import org.jivesoftware.smackx.xdata.FormField
 
-class GroupStatusAdapter(private val statuses: List<FormField.Option>,
+class GroupStatusAdapter(private val statuses: List<FormField.Option>, val groupchat: GroupChat,
                          private val descriptions: List<FormField>, val listener: Listener)
     : RecyclerView.Adapter<GroupStatusVH>(){
 
@@ -27,7 +29,9 @@ class GroupStatusAdapter(private val statuses: List<FormField.Option>,
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = GroupStatusVH(
-            LayoutInflater.from(parent.context).inflate(R.layout.group_status_item_vh, parent, false))
+            LayoutInflater.from(parent.context)
+                    .inflate(R.layout.group_status_item_vh, parent, false),
+            groupchat)
 
     interface Listener{
         fun onStatusClicked(option: FormField.Option)
@@ -35,12 +39,18 @@ class GroupStatusAdapter(private val statuses: List<FormField.Option>,
 
 }
 
-class GroupStatusVH(itemView: View): RecyclerView.ViewHolder(itemView){
+class GroupStatusVH(itemView: View, val groupchat: GroupChat): RecyclerView.ViewHolder(itemView){
 
     fun bind(status: FormField.Option, description: FormField, listener: Listener){
         itemView.findViewById<TextView>(R.id.group_status_item_tv).text = status.label
-        itemView.findViewById<ImageView>(R.id.group_status_item_iv).setImageLevel(
-                StatusMode.createStatusMode(description.values[0]).statusLevel)
+
+        val statusLevelOffset = if (groupchat.privacyType == GroupchatPrivacyType.INCOGNITO)
+            StatusMode.INCOGNITO_GROUP_OFFSET
+        else StatusMode.PUBLIC_GROUP_OFFSET
+
+        StatusBadgeSetupHelper.setupImageView(StatusMode.createStatusMode(description.values[0]),
+                statusLevelOffset, itemView.findViewById(R.id.group_status_item_iv))
+
         itemView.setOnClickListener { listener.onClick() }
         itemView.setOnLongClickListener {
             Toast.makeText(itemView.context, description.description, Toast.LENGTH_SHORT).show()
--- xabber/src/main/java/com/xabber/android/ui/fragment/groups/GroupStatusFragment.kt
+++ xabber/src/main/java/com/xabber/android/ui/fragment/groups/GroupStatusFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.xabber.android.R
 import com.xabber.android.data.Application
@@ -29,6 +30,9 @@ class GroupStatusFragment(val groupchat: GroupChat) : Fragment(), GroupStatusRes
                 container, false)
 
         recyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(context).apply {
+            orientation = LinearLayoutManager.VERTICAL
+        }
 
         return view
     }
@@ -48,48 +52,75 @@ class GroupStatusFragment(val groupchat: GroupChat) : Fragment(), GroupStatusRes
 
     override fun onStatusDataFormReceived(groupchat: GroupChat, dataForm: DataForm) {
         if (!isThisGroupChat(groupchat)) return
-        (activity as GroupStatusActivity).showProgressBar(false)
+        Application.getInstance().runOnUiThread {
+            (activity as GroupStatusActivity).showProgressBar(false)
 
-        this.dataForm = dataForm
-        val descriptions = mutableListOf<FormField>()
+            this.dataForm = dataForm
+            val descriptions = mutableListOf<FormField>()
 
-        for (field in dataForm.fields)
-            if (field.variable != "status" && field.type != FormField.Type.hidden && field.type != FormField.Type.fixed)
-                descriptions.add(field)
+            for (field in dataForm.fields)
+                if (field.variable != "status" && field.type != FormField.Type.hidden
+                        && !field.description.isNullOrEmpty())
+                    descriptions.add(field)
 
-        val adapter = GroupStatusAdapter(dataForm.getField("status").options,
-                descriptions, this)
+            val adapter = GroupStatusAdapter(dataForm.getField("status").options,
+                    groupchat, descriptions, this)
 
-        recyclerView.adapter = adapter
-        adapter.notifyDataSetChanged()
+            recyclerView.adapter = adapter
+            adapter.notifyDataSetChanged()
+        }
     }
 
     private fun isThisGroupChat(groupchat: GroupChat) = this.groupchat == groupchat
 
     override fun onError(groupchat: GroupChat) {
         if (!isThisGroupChat(groupchat)) return
-        (activity as GroupStatusActivity).showProgressBar(false)
-        GroupchatManager.getInstance().requestGroupStatusForm(groupchat)
-        Toast.makeText(context, R.string.groupchat_error, Toast.LENGTH_SHORT).show()
+        Application.getInstance().runOnUiThread {
+            (activity as GroupStatusActivity).showProgressBar(false)
+            GroupchatManager.getInstance().requestGroupStatusForm(groupchat)
+            Toast.makeText(context, R.string.groupchat_error, Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onStatusSuccessfullyChanged(groupchat: GroupChat) {
         if (!isThisGroupChat(groupchat)) return
-        (activity as GroupStatusActivity).showProgressBar(false)
-        activity?.finish()
+        Application.getInstance().runOnUiThread {
+            (activity as GroupStatusActivity).showProgressBar(false)
+            activity?.finish()
+        }
+    }
+
+    private fun createNewDataForm(option: FormField.Option): DataForm {
+        val newDataForm = DataForm(DataForm.Type.submit).apply {
+            title = dataForm.title
+            instructions = dataForm.instructions
+        }
+
+        for (oldFormField in dataForm!!.fields) {
+
+            if (oldFormField.variable == null) continue
+
+            val formFieldToBeAdded = FormField(oldFormField.variable).apply {
+                type = oldFormField.type
+                label = oldFormField.label
+                description = oldFormField.description
+            }
+
+            if (oldFormField.variable == "status")
+                formFieldToBeAdded.addValue(option.value)
+            else formFieldToBeAdded.addValue(oldFormField.values[0])
+
+            newDataForm.addField(formFieldToBeAdded)
+        }
+
+        return newDataForm
+
     }
 
     override fun onStatusClicked(option: FormField.Option) {
         (activity as GroupStatusActivity).showProgressBar(true)
 
-        for (field in dataForm.fields) {
-            if (field.variable == "status") {
-                field.options.clear()
-                field.values[0] = option.value
-            }
-        }
-
-        GroupchatManager.getInstance().sendSetGroupchatStatusRequest(groupchat, dataForm)
+        GroupchatManager.getInstance().sendSetGroupchatStatusRequest(groupchat, createNewDataForm(option))
     }
 
 }
