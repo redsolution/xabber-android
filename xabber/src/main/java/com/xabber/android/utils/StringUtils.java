ab50cad83c736c9c32449452a36a66879f076d4a
Vitaly Popov <perdovorotov@gmail.com>
2021-03-29 15:14:48 +0500

    Fixed get human geadable bytes

--- xabber/src/main/java/com/xabber/android/utils/StringUtils.java
+++ xabber/src/main/java/com/xabber/android/utils/StringUtils.java
@@ -384,12 +384,12 @@ public class StringUtils {
         if (-1000 < bytes && bytes < 1000) {
             return bytes + " B";
         }
-        CharacterIterator ci = new StringCharacterIterator("kMGTPE");
+        CharacterIterator ci = new StringCharacterIterator("KMGTPE");
         while (bytes <= -999_950 || bytes >= 999_950) {
-            bytes /= 1000;
+            bytes /= 1024;
             ci.next();
         }
-        return String.format(Locale.getDefault(), "%.1f %cB", bytes / 1000.0, ci.current());
+        return String.format(Locale.getDefault(), "%.2f %sB", bytes / 1024.0, ci.current() + "i");
     }
 
     /**
