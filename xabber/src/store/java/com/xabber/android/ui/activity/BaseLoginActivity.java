6996ce1efaa0e9070737515ab2f11a4ddfd99d1a
Vitaly Popov <perdovorotov@gmail.com>
2021-09-22 16:39:55 +0500

    Fixed AccountManager rewriting

--- xabber/src/store/java/com/xabber/android/ui/activity/BaseLoginActivity.java
+++ xabber/src/store/java/com/xabber/android/ui/activity/BaseLoginActivity.java
@@ -237,7 +237,7 @@ public abstract class BaseLoginActivity extends ManagedActivity implements
                         updateAccountInfo(s);
 
                         // if exist local accounts
-                        if (AccountManager.getInstance().getAllAccountItems().size() > 0)
+                        if (AccountManager.INSTANCE.getAllAccountItems().size() > 0)
                             updateSettings(needGoToMainActivity);
                         else getSettings(needGoToMainActivity);
 
